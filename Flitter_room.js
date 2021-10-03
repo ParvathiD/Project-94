2f8dc057e729212f75b6f188e817ecf41ecfabd7
ParvathiD <82469728+ParvathiD@users.noreply.github.com>
2021-10-03 13:26:45 -0500

    Add files via upload

---
+++ Flitter_room.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDCGcFCpIVHF9X-wDysSZcNCvnC6hkyeCc",
+    authDomain: "flitter-4346b.firebaseapp.com",
+    projectId: "flitter-4346b",
+    storageBucket: "flitter-4346b.appspot.com",
+    messagingSenderId: "403697605283",
+    appId: "1:403697605283:web:a5847fffab9fb601e7fd16",
+    measurementId: "G-K76CG5KTS0"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
